76346ab53912565e543bcfd9ca6bdc141b6d70c2
Chethan kumar H R <chethankumarhr751@gmail.com>
2026-06-28 16:46:25 +0530

    sitemap updated

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://chethanhrx.vercel.app';
+  const baseUrl = 'https://hethanhrx.netlify.app';
 
   return [
     {
